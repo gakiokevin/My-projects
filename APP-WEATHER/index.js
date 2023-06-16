a87ac91b7ce06c28291b3e994d5c5086fbb2a04c
gakiokevin5@gmail.com <gakiokevin5@gmail.com>
2023-06-16 13:59:30 +0300

    A weather App

---
+++ APP-WEATHER/index.js
@@ -0,0 +1,50 @@
+
+let cityInput,searchBtn,weatherIcon,humidity,windSpeed,
+temp,city;
+  cityInput= document.getElementById('cityInput')
+  searchBtn = document.getElementById('searchBtn')
+  weatherIcon =document.getElementById('weatherIcon')
+  humidity =document.querySelector('.humidity')
+  windSpeed =document.querySelector('.windSpeed')
+  city =document.getElementById('city')
+  temp = document.getElementById('temp')
+//   console.log(cityInput)
+//   console.log(searchBtn)
+//   console.log(weatherIcon)
+//   console.log(humidity)
+//   console.log(windSpeed)
+//   console.log(temp)
+//   console.log(city)
+
+    searchBtn.addEventListener('click',weatherInfo)
+     const URL = "./speed.png"
+     weatherIcon.src =URL
+    async function weatherInfo(){
+      cityInput= document.getElementById('cityInput')
+      cityInput = cityInput.value
+      if(cityInput.trim()!==""){
+       const WEATHERURL = `http://api.weatherapi.com/v1/current.json?key=15ff0e78d5fa4aa8b27121057231206&q=${cityInput}&aqi=no`
+       try{
+            let response =  await fetch(WEATHERURL,{mode:"cors"})
+            let weatherResponse = await response.json()
+            if(response.status===200){
+              // console.log(weatherResponse.current)
+            let locationName =weatherResponse.location.name
+             city.textContent = locationName.toUpperCase()
+             temp.textContent = `${weatherResponse.current.temp_c}c`
+             humidity.textContent = weatherResponse.current.humidity
+             windSpeed.textContent = `${weatherResponse.current.wind_kph} km/hr`
+             weatherIcon.src = weatherResponse.current.condition.icon
+
+
+            }else {
+               alert(weatherResponse.error.message)
+            }
+       } catch(error){
+         console.log(error)
+       }
+      } else {
+         alert('Enter a city name')
+      } 
+    }
+     
